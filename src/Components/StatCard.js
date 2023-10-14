a353a9ea72bd345cfd7ee6d5c377e2a02e438716
hargun0360 <hargunsingh0720@gmail.com>
2023-10-14 19:57:58 +0530

    Add: Stats cards at dashboard

---
+++ src/Components/StatCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const StatCard = ({ icon, title, value, change , id}) => (
+  <div className="stat-card">
+    <div className={"icon-container" + id}>
+      <i className={icon}></i>  
+    </div>
+
+    <div className="title">{title}</div>
+
+    <div className="content">
+      <div className="value">${value}</div>
+      <div className="change">{change}</div>
+    </div>
+  </div>
+);
+
+export default StatCard;
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,53 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar'
-const Dashboard = () => {
+import React from 'react';
+import StatCard from '../../Components/StatCard';
+import Sidebar from '../../Components/Sidebar';
+
+const cardsData = [
+    { icon: "fa-solid fa-money-bill", title: "Total Revenues", value: "2,129,430", change: "+2.5%" , id : "1" },
+    { icon: "fa-solid fa-tags", title: "Total Transactions", value: "1,520", change: "+1.7%" , id : "2" },
+    { icon: "fa-regular fa-thumbs-up", title: "Total Likes", value: "9,721", change: "+1.4%" , id : "3" },
+    { icon: "fa-solid fa-user-group", title: "Total Users", value: "9,721", change: "+4.2%" , id : "4" }
+  ];
+
+const DashboardContent = () => {
   return (<>
- 
+  <div className="Dashboard-container">
     <Sidebar />
-  </>
-  )
+    <div className="dashboard-content">
+      <header>
+        <h2>Dashboard</h2>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>🔍</button>
+          <button className="profile">👤</button>
+        </div>
+      </header>
+      <section className="stats">
+        {
+            cardsData.map((card) => (
+                <StatCard {...card} />
+            ))
+        }
+      </section>
+      <section className="activities">
+        <h3>Activities</h3>
+        
+        <div className="content-placeholder"></div>
+      </section>
+
+      <section className="top-products">
+        <h3>Top Products</h3>
+       
+        <div className="content-placeholder"></div>
+      </section>
+
+      <section className="add-profile">
+        <button className="add-button">+</button>
+        <span>Add Profile</span>
+      </section>
+    </div>
+    </div>
+    </>);
 }
 
-export default Dashboard
+export default DashboardContent;
